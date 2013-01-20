0b4f4d82b383c88d4cba10e9caea3147f516064e
Artur Gavkalyuk <mandeburka@gmail.com>
2013-01-20 10:05:53 +0200

    ticket:5 add tests

--- mandeburka_test/contact/tests.py
+++ mandeburka_test/contact/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from mandeburka_test.contact.models import Request
+from random import randint
 
 
 class ContactTest(TestCase):
@@ -12,15 +13,16 @@ class ContactTest(TestCase):
             self.fail('user not found')
 
     def test_main_page(self):
+        user = User.objects.get(username='admin')
         response = self.client.get('/')
-        self.assertContains(response, 'Artur')
-        self.assertContains(response, 'Gavkaliuk')
+        self.assertContains(response, user.first_name)
+        self.assertContains(response, user.last_name)
         self.assertContains(response, '14.02.1987')
-        self.assertContains(response, 'Email: admin@mail.com')
-        self.assertContains(response, 'Jabber: test@jabber.com')
-        self.assertContains(response, 'My Bio')
-        self.assertContains(response, 'My other contacts')
-        self.assertContains(response, 'Skype: skype_id_test')
+        self.assertContains(response, 'Email: %s' % user.email)
+        self.assertContains(response, 'Jabber: %s' % user.userprofile.jabber)
+        self.assertContains(response, user.userprofile.bio)
+        self.assertContains(response, user.userprofile.other_contacts)
+        self.assertContains(response, 'Skype: %s' % user.userprofile.skype)
 
     def test_request_middleware(self):
         response = self.client.get('/')
@@ -50,3 +52,42 @@ class ContactTest(TestCase):
         self.assertIn('settings', response.context)
         settings = response.context['settings']
         self.assertEquals(settings.TIME_ZONE, 'Europe/Kiev')
+
+    def test_secure_page(self):
+        response = self.client.get('/edit')
+        # redirect anonymous user to login page
+        self.assertEquals(response.status_code, 302)
+        self.assertEqual(resp['Location'], 'http://testserver/login')
+        # authenticated user can see edit page
+        self.client.login(username='admin', password='admin')
+        response = self.client.get('/edit')
+        self.assertEquals(response.status_code, 200)
+
+    def test_edit_form(self):
+        self.client.login(username='admin', password='admin')
+        user = User.objects.get(username='admin')
+        # valid form submission should update user record
+        data = {
+            'first_name': '%s_%d' % (user.first_name, randint(0, 100)),
+            'last_name': '%s_%d' % (user.last_name, randint(0, 100)),
+            'date_of_birth': '%02d.%02d.%d' %
+            (randint(1, 28), randint(1, 12,), randint(1900, 2000)),
+            'bio': '%s_%d' % (user.userprofile.bio, randint(0, 100)),
+            'jabber': '%s_%d' % (user.userprofile.jabber, randint(0, 100)),
+            'skype': '%s_%d' % (user.userprofile.skype, randint(0, 100)),
+            'other_contacts': '%s_%d' %
+            (user.userprofile.other_contacts, randint(0, 100)),
+        }
+        response = self.client.post('/edit', data)
+        self.assertEquals(response.status_code, 302)
+        self.assertEqual(resp['Location'], 'http://testserver/')
+        # reload user
+        user = User.objects.get(username='admin')
+        for k, v in data.iteritems():
+            if k in ('first_name', 'last_name'):
+                user_value = getattr(user, k)
+            else:
+                user_value = getattr(user.userprofile, k)
+            if k == 'date_of_birth':
+                user_value = user_value.strftime('%d.%m.%Y')
+            self.assertEquals(v, user_value)
