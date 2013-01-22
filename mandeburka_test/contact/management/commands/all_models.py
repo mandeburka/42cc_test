0368dc334ed65d59f23bc5822e8d3628493d7012
Artur Gavkalyuk <mandeburka@gmail.com>
2013-01-22 16:23:18 +0200

    ticket:9 handle DatabaseError exception

--- mandeburka_test/contact/management/commands/all_models.py
+++ mandeburka_test/contact/management/commands/all_models.py
@@ -1,15 +1,18 @@
 from django.core.management.base import BaseCommand
 from django.contrib.contenttypes.models import ContentType
-
+from django.db.utils import DatabaseError
 
 class Command(BaseCommand):
     help = 'Print all project models and their quantity in database'
 
     def handle(self, *args, **options):
-        for model_type in ContentType.objects.all():
-            s = '%s_%s - %d\n' % (
-                model_type.app_label,
-                model_type.model,
-                len(model_type.model_class().objects.all()))
-            self.stdout.write(s)
-            self.stderr.write('Error: %s' % s)
+        try:
+            for model_type in ContentType.objects.all():
+                s = '%s_%s - %d\n' % (
+                    model_type.app_label,
+                    model_type.model,
+                    len(model_type.model_class().objects.all()))
+                self.stdout.write(s)
+                self.stderr.write('Error: %s' % s)
+        except DatabaseError:
+            pass
